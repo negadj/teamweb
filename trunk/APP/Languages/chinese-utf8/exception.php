25e4ef3ca61a8c6a8b450a4b6fd663d5f936b14f
xuchangyuhui@sohu.com <xuchangyuhui@sohu.com@f4eb628c-b7a3-11dd-a80a-757f205f25d6>
2008-11-22 07:11:22 +0000

    added

---
+++ trunk/APP/Languages/chinese-utf8/exception.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 定义 Team web 界面的信息字符串
+ *
+ * @package TeamWeb
+ * @subpackage Languages
+ * @author  Zhou Yuhui (xuchangyuhui@sohu.com)
+ * @version 1.0, 2008-11-22
+ */
+return array(
+    'ex_data_not_found' => '指定的数据成员 [ID = %s] 没有找到',
+	'ex_image_failed' => '图像操作 "%s" 发生错误',
+);
+
+?>
