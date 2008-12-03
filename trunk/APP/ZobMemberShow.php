d4bb81f5bc448e4428b275b96a8e8c66107f98a3
xuchangyuhui@sohu.com <xuchangyuhui@sohu.com@f4eb628c-b7a3-11dd-a80a-757f205f25d6>
2008-12-03 04:23:23 +0000

    update photo showing.

--- trunk/APP/ZobMemberShow.php
+++ trunk/APP/ZobMemberShow.php
@@ -1,8 +1,10 @@
 <?php include 'ZobHeader.php' ?>
 <div id=home_cont class="home_cont">
   <div style="float:left; width:35%">
+    <a href="<?php echo FLEA::getAppInf('uploadRoot') . '/' . $member['photo']; ?>"
+       target="blank">
      <img src="<?php echo FLEA::getAppInf('uploadRoot') . '/' . $member['photo']; ?>" 
-          border="0" width=300 />
+        border="0" width=300 /></a>
   </div>
   <div style="float:right; width:55%">
     <table class="memberblock">
